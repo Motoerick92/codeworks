bf1629f6e2b9464a699bba803e992c8cfa95cf54
Erick Garcia <ercik_mora92@hotmail.com>
2024-12-27 22:33:35 -0600

    static

--- Settings/settings.py
+++ Settings/settings.py
@@ -129,6 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 if not DEBUG:
     # Tell Django to copy statics to the `staticfiles` directory
