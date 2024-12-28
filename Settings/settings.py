04d5fb28c71f5436db0ca4c1c4ead09255e1f1d6
Erick Garcia <ercik_mora92@hotmail.com>
2024-12-27 22:43:46 -0600

    home - whitenoise

--- Settings/settings.py
+++ Settings/settings.py
@@ -60,6 +60,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'Settings.urls'
@@ -142,6 +144,8 @@ if not DEBUG:
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
+
+LOGIN_URL = '/home'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
